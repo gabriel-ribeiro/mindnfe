fc4821006a710a129fd83beb66faec0c82e1e575
Gabriel Ribeiro Araujo <contato@gabrielribeiro.net>
2013-01-03 11:19:05 -0400

    Initial Commit

--- Notafiscal.php
+++ Notafiscal.php
@@ -2,9 +2,29 @@
 
 	namespace MindNFE;
 	
-	
+	/**
+	 *	Classe Nota Fiscal
+	 *	Desenvolvida para guardar os dados básicos da nota fiscal eletrônica (NFe).	
+	 *	De acordo com o Manual da SEFAZ
+	 *	
+	 *	@license	MIT <http://opensource.org/licenses/mit-license.php>
+	 *	@package	MindNFE
+	 *	@version	1.0
+	 *	@author		Gabriel Ribeiro Araújo <contato@gabrielribeiro.net>
+	 */
 	class Notafiscal {
+
+		/**
+		 * Versão do layout (v2.0)
+		 * @var float
+		 */
+		private $schema_version = 2.0;
 		
-		
+		/**
+		 * Identificador da TAG a ser assinada
+		 * Informar a chave de acesso da NF-e precedida do literal "NFe" acrescentada a validação do formato (v2.0).
+		 * @var string
+		 */
+		private $id = '';
 		
 	}
